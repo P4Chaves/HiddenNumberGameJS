7262fbf923adacdaa01bb123957c2d9334de3b3b
P4 <kogaaltme@gmail.com>
2025-02-12 11:06:03 -0300

    Fleshing out the prompt.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 alert('Starting the hidden number game.');
+let hiddenNumber = 4;
+let guess = prompt('Choose a number between 1 and 10:');
+
+if (hiddenNumber == guess) {
+    console.log('Nice! You found the hidden number (4)');
+}
