b741cd341bb7e42ee662d14ceb8b5f1c37e44657
P4 <kogaaltme@gmail.com>
2025-02-19 11:04:36 -0300

    Cleaning code and adding user hints.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 alert('Starting the hidden number game.');
 let hiddenNumber = 4;
-console.log (hiddenNumber)
-let guess = prompt('Choose a number between 1 and 10:');
+console.log(hiddenNumber);
 
-if (hiddenNumber == guess) {
+let guess = Number(prompt('Choose a number between 1 and 10:'));
+
+if (hiddenNumber === guess) {
     alert(`Nice! You found the hidden number: ${hiddenNumber}`);
+} else if (hiddenNumber > guess) {
+    alert(`The hidden number is greater than ${guess}`);
+} else {
+    alert(`The hidden number is smaller than ${guess}`);
 }
-else (alert('Wrong number, try again.'))
