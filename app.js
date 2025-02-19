2fe447a10fdb7683ea77af5dcd54df0b2b26fd01
P4 <kogaaltme@gmail.com>
2025-02-19 11:13:58 -0300

    Cleaning code once again and adding more tries to the player.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ alert('Starting the hidden number game.');
 let hiddenNumber = 4;
 console.log(hiddenNumber);
 
-let guess = Number(prompt('Choose a number between 1 and 10:'));
+let guess = parseInt(prompt('Choose a number between 1 and 10:'));
 
-if (hiddenNumber === guess) {
-    alert(`Nice! You found the hidden number: ${hiddenNumber}`);
-} else if (hiddenNumber > guess) {
-    alert(`The hidden number is greater than ${guess}`);
-} else {
-    alert(`The hidden number is smaller than ${guess}`);
+while (guess !== hiddenNumber) {
+    if (hiddenNumber > guess) {
+        alert(`The hidden number is greater than ${guess}`);
+    } else {
+        alert(`The hidden number is smaller than ${guess}`);
+    }
+    guess = parseInt(prompt('Choose a number between 1 and 10:'));
 }
+
+alert(`Nice! You found the hidden number: ${hiddenNumber}`);
