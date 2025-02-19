0ce2a41905c1b04a1708a9c135160ca3b5a6492b
P4 <kogaaltme@gmail.com>
2025-02-19 11:30:13 -0300

    Adding attempts to the code and informing them to the player.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ alert('Starting the hidden number game.');
 let hiddenNumber = 4;
 console.log(hiddenNumber);
 
+let attempts = 0;
 let guess = parseInt(prompt('Choose a number between 1 and 10:'));
+attempts++;
 
 while (guess !== hiddenNumber) {
     if (hiddenNumber > guess) {
@@ -11,6 +13,7 @@ while (guess !== hiddenNumber) {
         alert(`The hidden number is smaller than ${guess}`);
     }
     guess = parseInt(prompt('Choose a number between 1 and 10:'));
+    attempts++;
 }
 
-alert(`Nice! You found the hidden number: ${hiddenNumber}`);
+alert(`Nice! You found the hidden number: ${hiddenNumber} in ${attempts} attempts.`);
